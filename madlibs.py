03d8fc81f4a4493badcf321e82abe4ce60a4d861
laceyrocks <lvolterm7843@hwdsb.on.ca>
2025-09-15 08:08:27 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+#the inputs
+
+adj = input("Enter an adjective:")
+name = input("Enter a name:")
+badfood = input("Enter a disgusting food, but non-plural:")
+restaurant = input("Enter a fast food place:")
+animals = input("Enter an animal, but in its plural form:")
+number = int(input("Enter a number:"))
+adj2 = input("Enter an adjective to describe food:")
+
+
+
+#the story
+
+print("On a bright and", adj, " afternoon,", name, "just got off from work.")
+print("They were so excited, especially after they accidentally ate that nasty", badfood, "last night.")
+print("They were driving to their favourite spot", restaurant)
+print("Suddenly, a massive amount of", animals, "blocked the road. They scared", name)
+print("Although late,", name, "eventually arrived at their destination.")
+print("They ordered", number*2, "coffees and drank them all. They tasted", adj2)
+print("Suddenly, they realized that the coffee had milk in it.")
+print("They were lactose intolerant.")
